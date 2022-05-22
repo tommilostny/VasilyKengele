c163562bad64e65f7108655ff63934caa134ee47
tommilostny <tommilostny@live.com>
2022-05-22 22:20:18 +0200

    Updated users_count message formating.

--- src/Handlers/VKTelegramBotHandler.cs
+++ src/Handlers/VKTelegramBotHandler.cs
@@ -138,12 +138,13 @@ public class VKTelegramBotHandler
         var wakingUp = users.Count(u => u.ReceiveWakeUps);
         var notWakingUp = users.Count(u => !u.ReceiveWakeUps);
 
-        var messageBuilder = new StringBuilder($"Right now {wakingUp} users are waking up with Vasily Kengele!")
+        var messageBuilder = new StringBuilder($"Right now <b>{wakingUp}</b> users are waking up with Vasily Kengele!")
             .AppendLine()
-            .AppendLine($"And {notWakingUp} registered users are not waking up with us.");
+            .AppendLine($"And <b>{notWakingUp}</b> of the registered users are not waking up with us.");
 
         await botClient.SendTextMessageAsync(user.ChatId,
             text: messageBuilder.ToString(),
+            parseMode: ParseMode.Html,
             cancellationToken: cancellationToken);
     }
 
