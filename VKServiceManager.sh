ecf94bf9c284f71e2fbf9dc3c5692b959211a893
tommilostny <tommilostny@live.com>
2022-06-23 18:58:21 +0200

    Added shell script to solution + made an update script action.

--- VKServiceManager.sh
+++ VKServiceManager.sh
@@ -5,22 +5,36 @@ DOTNET=$HOME_FOLDER/.dotnet/dotnet
 
 compileVK()
 {
+	echo "Compiling Vasily Kengele..."
 	$DOTNET publish -c Release $VK_FOLDER/src/VasilyKengele.csproj
+}
+
+startVK()
+{
+	echo "Starting and enabling the service on boot."
+	systemctl enable VasilyKengele.service
+	systemctl start VasilyKengele.service
+}
+
+stopVK()
+{
+	echo "Stopping and disabling the service."
+	systemctl stop VasilyKengele.service
+	systemctl disable VasilyKengele.service
 }
 
 # Copy only if service file does not exist already or is updated.
 cp --update $VK_FOLDER/VasilyKengele.service /etc/systemd/system/
 
-echo "What do you want to do with Vasily Kengele service? (start/stop)"
+echo "What do you want to do with Vasily Kengele service? (start/stop/update)"
 read action
 
 if [[ $action == "start" ]]
 then
 	# Compile Vasily Kengele C# project if needed.
-        if [[ ! -f $VK_FOLDER/src/bin/Release/net6.0/publish/VasilyKengele.dll ]]
-        then
-		echo "Compiling Vasily Kengele..."
-                compileVK
+    if [[ ! -f $VK_FOLDER/src/bin/Release/net6.0/publish/VasilyKengele.dll ]]
+    then
+        compileVK
 	else
 		echo "Vasily Kengele project is already compiled. Recompile? (y/n)"
 		read compile
@@ -29,14 +43,20 @@ then
 			compileVK
 		fi	
 	fi
-	echo "Starting and enabling the service on boot."
-	systemctl enable VasilyKengele.service
-	systemctl start VasilyKengele.service
+
+	startVK
+
 elif [[ $action == "stop" ]]
 then
-	echo "Stopping and disabling the service."
-	systemctl stop VasilyKengele.service
-	systemctl disable VasilyKengele.service
+	stopVK
+
+elif [[ $action == "update" ]]
+then
+	stopVK
+	git pull
+	compileVK
+	startVK
+
 else
 	echo "Invalid command!"
 	echo "Available commands: start, stop"
